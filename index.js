94da5f5e253ebacafd379c44b47f517e1bc749bb
hsnetzer <hsnetzer@gmail.com>
2019-02-25 14:34:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ fs.readFile(__dirname + '/' + process.argv[2], function(err, data) {
     parser.parseString(data, function (err, result) {
         let waypoints = result.gpx.wpt;
 
-        var waypointsOut = `<?xml version="1.0" encoding="UTF-8"?><gpx>`;
+        var waypointsOut = `<?xml version="1.0" encoding="UTF-8"?>
+<gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="ExpertGPS 6.11 using GPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.topografix.com/GPX/gpx_overlay/0/3 http://www.topografix.com/GPX/gpx_overlay/0/3/gpx_overlay.xsd http://www.topografix.com/GPX/gpx_modified/0/1 http://www.topografix.com/GPX/gpx_modified/0/1/gpx_modified.xsd">
+<metadata>
+<extensions>
+<time xmlns="http://www.topografix.com/GPX/gpx_modified/0/1">2018-01-07T15:09:35.573Z</time>
+</extensions>
+</metadata>`;
         for (var i = 0; i < waypoints.length; i++) {
           var waypoint = waypoints[i];
 
